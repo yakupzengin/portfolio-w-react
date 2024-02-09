e08798d3a73132a5c91de10930916db32137fd0d
Yakup Zengin <clafedersin@gmail.com>
2024-02-09 23:53:11 +0300

    added footer part

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import "./footer.css"
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__container container">
+                <h1 className="footer__title">Yakup</h1>
+
+                <ul className="footer__list">
+                    <li>
+                        <a href="#about" className='footer__link'>About</a>
+                    </li>
+                    <li>
+                        <a href="#skills" className='footer__link'>Skills</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio" className='footer__link'>Portfolyo</a>
+                    </li>
+                </ul>
+                <div className="footer__social">
+                        <a href="https://www.linkedin.com/in/yakupzengın/" 
+                        className="footer__social-link" 
+                        target='_blank'>
+                            <i className="bx bxl-linkedin"></i>
+                        </a>
+
+
+                        <a href="https://github.com/yakupzengin" 
+                        className="footer__social-link" 
+                        target='_blank'>
+                            <i class="bx bxl-github"></i>
+                        </a>
+                    </div>
+
+                    <span className="footer__copy">
+                        &#169; Yakup Zengin. All rights reserved
+                    </span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
