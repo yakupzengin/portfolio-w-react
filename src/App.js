9a7b336a5b319fedeb74b133cb4b203d11761872
Yakup Zengin <clafedersin@gmail.com>
2024-02-09 23:53:24 +0300

    Update app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Skills from './components/skills/Skills'
 import Services from './components/services/Services'
 import Work from './components/work/Work'
 import Contact from './components/contact/Contact'
+import Footer from "./components/footer/Footer"
 const App = () => {
   return (
     <>
@@ -18,6 +19,7 @@ const App = () => {
       <Services/>
       <Work/>
       <Contact/>
+      <Footer/>
     </main>
     </>
   )
